b1306b18fe6ad41de5a954169346d16c51c300b8
Valeria Cardona <valecar2104@gmail.com>
2025-10-24 17:19:42 -0500

    se modificó el archivo index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const phrases = [
 
 app.get('/', (req, res) => {
     const number = Math.floor(Math.random() * 7);
-    res.send(phrases[number])
+    res.send(phrases[number]+" - Container Id: "+os.hostname())
 })
 
 app.listen(port, () => {
