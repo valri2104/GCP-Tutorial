7043797fd0f1f415c913394419932a775fcb9255
Valeria Cardona <valecar2104@gmail.com>
2025-10-25 18:02:53 -0500

    c

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const phrases = [
     "See rejection as redirection.",
     "There is beauty in simplicity.",
     "You can’t be late until you show up.",
-    "Maybe life is testing you. Don’t give up.",
+    "Maybe life is testing you. Don’t give up",
     "Impossible is just an opinion.",
     "Alone or not you gonna walk forward.",
 ]
